39a30ff37b134354c9f2983d4e56e0d4e7aede35
TerroR75 <kamilsulimierski@gmail.com>
2023-01-31 13:35:14 +0100

    Added colors variables

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -3,7 +3,12 @@ import { useState } from 'react';
 function Post() {
   // const [count, setCount] = useState(0);
 
-  return <div className='post'>NEW POST</div>;
+  return (
+    <div className='post'>
+      <div className='post-karma-vote'>votes</div>
+      <div className='post-content'>Content</div>
+    </div>
+  );
 }
 
 export default Post;
